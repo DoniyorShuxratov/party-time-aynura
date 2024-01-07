dc1d9c0eaa982c16e2ba2a3daa8d6cef9e55e9f5
DoniyorShuxratov <shuxratovdoni@gmail.com>
2024-01-07 11:26:21 +0500

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  
+    window.addEventListener("scroll", function () {
+      // Show the button when scrolling down
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+      } else {
+        scrollToTopBtn.style.display = "none";
+      }
+    });
+  
+    scrollToTopBtn.addEventListener("click", function () {
+      // Scroll to the top smoothly
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    });
+  });
+  
+
 function showSidebar() {
     const sidebar = document.querySelector('.nav__navigation--flexed')
     sidebar.style.display = 'flex'
